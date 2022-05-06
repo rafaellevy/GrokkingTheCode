44fdc9d94ca0722247733f1ee34311a783cebe71
rafaellevy <srlevy2@gmail.com>
2022-05-06 13:28:16 -0700

    cycle detection done and tested

---
+++ fastAndSlow.py
@@ -0,0 +1,36 @@
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+
+def has_cycle(head):
+  # TODO: Write your code here
+    fast = head
+    slow = head
+
+    while fast != None and fast.next != None:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            return True
+    return False
+
+
+def main():
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+  head.next.next.next.next.next = Node(6)
+  print("LinkedList has cycle: " + str(has_cycle(head)))
+
+  head.next.next.next.next.next.next = head.next.next
+  print("LinkedList has cycle: " + str(has_cycle(head)))
+
+  head.next.next.next.next.next.next = head.next.next.next
+  print("LinkedList has cycle: " + str(has_cycle(head)))
+
+
+main()
