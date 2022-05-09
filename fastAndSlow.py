0dc9dc944f55a709c833d2034ddc5f5959bfbbf2
rafaellevy <srlevy2@gmail.com>
2022-05-09 12:35:14 -0700

    find_cycle_start done and tested

--- fastAndSlow.py
+++ fastAndSlow.py
@@ -72,3 +72,38 @@ def main():
 
 
 main()
+
+def find_cycle_start(head):
+  length = find_cycle_length(head)
+
+  p1 = head
+  p2 = head
+
+  for _ in range(length):
+    p2 = p2.next
+
+  while p1 != p2:
+    p1 = p1.next
+    p2 = p2.next
+
+  return p1
+
+def main():
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+  head.next.next.next.next.next = Node(6)
+
+  head.next.next.next.next.next.next = head.next.next
+  print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+
+  head.next.next.next.next.next.next = head.next.next.next
+  print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+
+  head.next.next.next.next.next.next = head
+  print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+
+
+main()
