bd1279a25352417e6c66eea01673023afc914e30
rafaellevy <srlevy2@gmail.com>
2022-05-09 12:19:58 -0700

    find_cycle_length done and tested

--- fastAndSlow.py
+++ fastAndSlow.py
@@ -34,3 +34,41 @@ def main():
 
 
 main()
+
+
+def find_cycle_length(head):
+  fast = head
+  slow = head
+  newPointer = slow
+  count = 0
+
+
+  while fast != None and fast.next != None:
+      fast = fast.next.next
+      slow = slow.next
+      if fast == slow:
+        newPointer = slow
+        break
+
+  while True:
+    newPointer = newPointer.next
+    count += 1
+    if newPointer == slow:
+      break
+  return count 
+
+def main():
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+  head.next.next.next.next.next = Node(6)
+  head.next.next.next.next.next.next = head.next.next
+  print("LinkedList cycle length: " + str(find_cycle_length(head)))
+
+  head.next.next.next.next.next.next = head.next.next.next
+  print("LinkedList cycle length: " + str(find_cycle_length(head)))
+
+
+main()
