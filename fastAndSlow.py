5217031cc1e57d694cc6ed01b71b1d2b4bc6c382
rafaellevy <srlevy2@gmail.com>
2022-05-10 12:43:29 -0700

    happy number and find middle done and tested

--- fastAndSlow.py
+++ fastAndSlow.py
@@ -107,3 +107,65 @@ def main():
 
 
 main()
+
+print("HAPPY NUMBER")
+
+def find_happy_number(num):
+  slow, fast = num, num
+  while True:
+    slow = find_square(slow)
+    fast = find_square(find_square(fast))
+    if slow == fast:
+      if slow == 1:
+        return True
+      else:
+        return False
+
+
+
+
+def find_square(num):
+  sum = 0
+  while num > 0:
+    last_digit = num % 10
+    sum += last_digit * last_digit
+    num = num // 10
+  return sum
+
+
+def main():
+  print(find_happy_number(23))
+  print(find_happy_number(12))
+
+
+main()
+
+print("FIND MIDDLE OF LINKED LIST")
+
+def find_middle_of_linked_list(head):
+  fast = head
+  slow = head
+  while fast != None and fast.next != None:
+    slow = slow.next
+    fast = fast.next.next
+
+  return slow
+
+
+def main():
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+
+  print("Middle Node: " + str(find_middle_of_linked_list(head).value))
+
+  head.next.next.next.next.next = Node(6)
+  print("Middle Node: " + str(find_middle_of_linked_list(head).value))
+
+  head.next.next.next.next.next.next = Node(7)
+  print("Middle Node: " + str(find_middle_of_linked_list(head).value))
+
+
+main()
