1537881fa8d99ffb2a44ef7a224d86da350a206e
rafaellevy <srlevy2@gmail.com>
2022-05-12 12:57:40 -0700

    Rearrange a LinkedList done and tested

--- fastAndSlow.py
+++ fastAndSlow.py
@@ -253,3 +253,64 @@ def main():
 
 main()
 
+
+
+# Rearrange a LinkedList (medium)
+
+'''
+Given the head of a Singly LinkedList, 
+write a method to modify the LinkedList 
+such that the nodes from the second half of 
+the LinkedList are inserted alternately to the 
+nodes from the first half in reverse order. 
+So if the LinkedList has nodes 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null,
+ your method should return 1 -> 6 -> 2 -> 5 -> 3 -> 4 -> null.
+
+Your algorithm should not use any extra space and the 
+input LinkedList should be modified in-place.
+
+Example 1:
+
+Input: 2 -> 4 -> 6 -> 8 -> 10 -> 12 -> null
+Output: 2 -> 12 -> 4 -> 10 -> 6 -> 8 -> null 
+Example 2:
+
+Input: 2 -> 4 -> 6 -> 8 -> 10 -> null
+Output: 2 -> 10 -> 4 -> 8 -> 6 -> null
+'''
+print("rearrangeLinkedList")
+def rearrangeLinkedList(head):
+  middleNode = find_middle_of_linked_list(head)
+  oneAfterMid = middleNode.next
+  middleNode.next = None
+  reversedList = reverseLinkedList(oneAfterMid)
+
+  firstHalf = head
+  secondHalf = reversedList
+
+
+  while secondHalf != None:
+    firstHalfNext = firstHalf.next
+    secondHalfNext = secondHalf.next
+    firstHalf.next = secondHalf
+    secondHalf.next = firstHalfNext
+    firstHalf = firstHalfNext
+    secondHalf = secondHalfNext
+
+  printLinkedList(head)
+  return head
+
+
+def main():
+  head = Node(2)
+  head.next = Node(4)
+  head.next.next = Node(6)
+  head.next.next.next = Node(8)
+  head.next.next.next.next = Node(10)
+  head.next.next.next.next.next = Node(12)
+  rearrangeLinkedList(head)
+  
+
+
+main()
+  
