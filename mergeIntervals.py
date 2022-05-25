3409416b0b223e861d9fe76f2d1498b54ce63053
rafaellevy <srlevy2@gmail.com>
2022-05-25 14:03:27 -0700

    Minimum Meeting Rooms using heap done and tested

--- mergeIntervals.py
+++ mergeIntervals.py
@@ -220,6 +220,7 @@ class Meeting:
     self.start = start
     self.end = end
 
+'''
 def min_meeting_rooms(meetings):
   meetings.sort(key=lambda x: x.start)
   rooms = []
@@ -257,5 +258,39 @@ def main():
 
 
 main()
+'''
 
+
+print("solution 2")
+# Minimum Meeting Rooms (hard) solution 2 ( better time complexity)
+
+def min_meeting_rooms(meetings):
+  # sort the meetings by start time
+  meetings.sort(key=lambda x: x.start)
+
+  minRooms = 0
+  minHeap = []
+  for meeting in meetings:
+    # remove all the meetings that have ended
+    while(len(minHeap) > 0 and meeting.start >= minHeap[0]):
+      heappop(minHeap)
+    # add the current meeting into min_heap
+    heappush(minHeap, meeting.end)
+    # all active meetings are in the min_heap, so we need rooms for all of them.
+    minRooms = max(minRooms, len(minHeap))
+  return minRooms
+
+
+def main():
+  print("Minimum meeting rooms required: " +
+        str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 5), Meeting(7, 9)])))
+  print("Minimum meeting rooms required: " +
+        str(min_meeting_rooms([Meeting(6, 7), Meeting(2, 4), Meeting(8, 12)])))
+  print("Minimum meeting rooms required: " +
+        str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 3), Meeting(3, 6)])))
+  print("Minimum meeting rooms required: " + str(min_meeting_rooms(
+    [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)])))
+
+
+main()
 
