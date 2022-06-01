53b1ca669d76259b81aeb8fc472f38a2bf3388b7
rafaellevy <srlevy2@gmail.com>
2022-06-01 12:00:05 -0700

    Employee Free Time done and tested

--- mergeIntervals.py
+++ mergeIntervals.py
@@ -360,3 +360,85 @@ def main():
 main()
 
 # Employee Free Time
+
+'''
+For K employees, we are given a list of intervals representing the working hours of each employee. Our goal is to find out if there is a free interval that is common to all employees. You can assume that each list of employee working hours is sorted on the start time.
+
+Example 1:
+
+Input: Employee Working Hours=[[[1,3], [5,6]], [[2,3], [6,8]]]
+Output: [3,5]
+Explanation: Both the employees are free between [3,5].
+Example 2:
+
+Input: Employee Working Hours=[[[1,3], [9,12]], [[2,4]], [[6,8]]]
+Output: [4,6], [8,9]
+Explanation: All employees are free between [4,6] and [8,9].
+Example 3:
+
+Input: Employee Working Hours=[[[1,3]], [[2,4]], [[3,5], [7,9]]]
+Output: [5,7]
+Explanation: All employees are free between [5,7].
+
+'''
+
+class Interval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def print_interval(self):
+        print("[" + str(self.start) + ", " + str(self.end) + "]", end='')
+
+
+def find_employee_free_time(schedule):
+    result = []
+    intervals = []
+    
+    for shifts in schedule:
+      for time in shifts:
+        intervals.append(time)
+
+    intervals.sort(key=lambda x: x.start)
+
+    lastEnd = intervals[0].end
+    for i in range(1,len(intervals)):
+      if intervals[i].start > lastEnd:
+        result.append(Interval(lastEnd,intervals[i].start))
+      lastEnd = max(lastEnd,intervals[i].end)
+
+    return result 
+      
+
+
+
+
+
+
+
+
+def main():
+
+    input = [[Interval(1, 3), Interval(5, 6)], [
+        Interval(2, 3), Interval(6, 8)]]
+    print("Free intervals: ", end='')
+    for interval in find_employee_free_time(input):
+        interval.print_interval()
+    print()
+
+    input = [[Interval(1, 3), Interval(9, 12)], [
+        Interval(2, 4)], [Interval(6, 8)]]
+    print("Free intervals: ", end='')
+    for interval in find_employee_free_time(input):
+        interval.print_interval()
+    print()
+
+    input = [[Interval(1, 3)], [
+        Interval(2, 4)], [Interval(3, 5), Interval(7, 9)]]
+    print("Free intervals: ", end='')
+    for interval in find_employee_free_time(input):
+        interval.print_interval()
+    print()
+
+
+main()
