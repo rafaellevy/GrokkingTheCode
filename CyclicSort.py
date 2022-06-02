73cf63366c02200957ed08acaaef3bc19e37e9b2
rafaellevy <srlevy2@gmail.com>
2022-06-02 12:27:04 -0700

    new versions for sort and missing number done

--- CyclicSort.py
+++ CyclicSort.py
@@ -39,6 +39,29 @@ def main():
 
 main()
 
+# Cyclic Sort ( version 2 - better ) 
+
+def cyclic_sort2(nums):
+    i = 0
+    while i < len(nums):
+        j = nums[i]
+        if j != i + 1:
+            swap(i,nums[i]-1,nums)
+        else:
+            i += 1
+    return nums
+
+def swap(x,y,arr):
+    arr[x], arr[y] = arr[y], arr[x]
+
+
+def main():
+  print(cyclic_sort2([3, 1, 5, 4, 2]))
+  print(cyclic_sort2([2, 6, 4, 3, 1, 5]))
+  print(cyclic_sort2([1, 5, 6, 4, 3, 2]))
+
+main()
+
 # Find the Missing Number (easy)
 
 '''
@@ -65,6 +88,39 @@ def find_missing_number(nums):
     for j in range(len(auxArray)):
         if auxArray[j] == None:
             return j
+
+
+
+def main():
+  print(find_missing_number([4, 0, 3, 1]))
+  print(find_missing_number([8, 3, 5, 2, 4, 6, 0, 1]))
+
+
+main()
+
+
+# Find the Missing Number (version 2 - Better)
+
+def find_missing_number(nums):
+    i = 0
+    while i < len(nums):
+        j = nums[i]
+        if j != i and j < len(nums):
+            swap(i,j,nums)
+        else:
+            i += 1
+        
+    for i in range(0,len(nums)):
+        if i != nums[i]:
+            return i
+            
+    return len(nums)
+        
+def swap(x,y,arr):
+    arr[x], arr[y] = arr[y], arr[x]
+
+
+
 
 
 
