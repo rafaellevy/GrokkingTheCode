48d7064ab9384327d99af3a46370914a30926c90
rafaellevy <srlevy2@gmail.com>
2022-06-02 12:59:57 -0700

    missing numbers done and tested

--- CyclicSort.py
+++ CyclicSort.py
@@ -112,8 +112,7 @@ def find_missing_number(nums):
         
     for i in range(0,len(nums)):
         if i != nums[i]:
-            return i
-            
+            return i    
     return len(nums)
         
 def swap(x,y,arr):
@@ -127,6 +126,52 @@ def swap(x,y,arr):
 def main():
   print(find_missing_number([4, 0, 3, 1]))
   print(find_missing_number([8, 3, 5, 2, 4, 6, 0, 1]))
+
+
+main()
+
+# Find all Missing Numbers 
+
+'''
+We are given an unsorted array containing numbers taken from the range 1 to ‘n’. The array can have duplicates, which means some numbers will be missing. Find all those missing numbers.
+
+Example 1:
+
+Input: [2, 3, 1, 8, 2, 3, 5, 1]
+Output: 4, 6, 7
+Explanation: The array should have all numbers from 1 to 8, due to duplicates 4, 6, and 7 are missing.
+Example 2:
+
+Input: [2, 4, 1, 2]
+Output: 3
+Example 3:
+
+Input: [2, 3, 2, 1]
+Output: 4
+
+'''
+
+def find_missing_numbers(nums):
+    missingNumbers = []
+    i = 0
+    while i < len(nums):
+        j = nums[i]
+        if j != i + 1 and j != nums[j -1]:
+            swap(i,j-1, nums)
+        else:
+            i += 1
+    for i in range(len(nums)):
+        if nums[i] == i + 1:
+            continue
+        else:
+            missingNumbers.append(i+1)
+
+    return missingNumbers
+
+def main():
+  print(find_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1]))
+  print(find_missing_numbers([2, 4, 1, 2]))
+  print(find_missing_numbers([2, 3, 2, 1]))
 
 
 main()
