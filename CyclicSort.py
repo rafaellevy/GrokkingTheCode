9897277a93db527c1f1f28c4b04b4f6eabe545d7
rafaellevy <srlevy2@gmail.com>
2022-06-03 13:00:59 -0700

    find_first_smallest_missing_positive done and test

--- CyclicSort.py
+++ CyclicSort.py
@@ -321,3 +321,58 @@ def main():
 
 
 main()
+
+
+# Find the Smallest Missing Positive Number (medium)#
+
+'''Given an unsorted array containing numbers, 
+find the smallest missing positive number in it.
+
+Note: Positive numbers start from 1.
+
+Example 1:
+
+Input: [-3, 1, 5, 4, 2]
+Output: 3
+Explanation: The smallest missing positive number is '3'
+Example 2:
+
+Input: [3, -2, 0, 1, 2]
+Output: 4
+Example 3:
+
+Input: [3, 2, 5, 1]
+Output: 4
+'''
+
+def find_first_smallest_missing_positive(nums):
+    i = 0
+    
+    while i < len(nums):
+        value = nums[i]
+        if value != i + 1:
+            if value < 1 or value > len(nums):
+                i +=1
+            else:
+                swap(i, value - 1, nums)
+        else:
+            i += 1
+
+    for i in range(len(nums)):
+        value = nums[i]
+        if value != i + 1:
+            return i + 1
+
+
+    
+def swap(x,y,arr):
+    arr[x], arr[y] = arr[y], arr[x]
+
+
+def main():
+  print(find_first_smallest_missing_positive([-3, 1, 5, 4, 2]))
+  print(find_first_smallest_missing_positive([3, -2, 0, 1, 2]))
+  print(find_first_smallest_missing_positive([3, 2, 5, 1]))
+
+
+main()
