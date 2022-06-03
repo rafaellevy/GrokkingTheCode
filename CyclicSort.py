9386e0ec98620cb23625b54c05ed98c14bca7ad3
rafaellevy <srlevy2@gmail.com>
2022-06-03 11:47:30 -0700

    find duplicate done and tested

--- CyclicSort.py
+++ CyclicSort.py
@@ -175,3 +175,56 @@ def main():
 
 
 main()
+
+
+# Find the Duplicate Number 
+
+
+'''
+We are given an unsorted array containing n+1 numbers taken from the range 1 to n. 
+The array has only one duplicate but it can be repeated multiple times. 
+Find that duplicate number without using any extra space. 
+You are, however, allowed to modify the input array.
+
+Example 1:
+
+Input: [1, 4, 4, 3, 2]
+Output: 4
+Example 2:
+
+Input: [2, 1, 3, 3, 5, 4]
+Output: 3
+Example 3:
+
+Input: [2, 4, 1, 4, 4]
+Output: 4
+
+
+'''
+
+def find_duplicate(nums):
+    i = 0
+
+    while True:
+        if nums[i] == i + 1:
+            i += 1
+        if nums[i] != i + 1:
+            value = nums[i]
+            if nums[value - 1] != nums[i]:
+                swap(i,value - 1,nums)
+            else:
+                return value
+
+def swap(x,y,arr):
+    arr[x], arr[y] = arr[y], arr[x]
+
+
+
+print("find duplicate - for sure we have a duplicate")
+def main():
+    print(find_duplicate([1, 4, 4, 3, 2]))
+    print(find_duplicate([2, 1, 3, 3, 5, 4]))
+    print(find_duplicate([2, 4, 1, 4, 4]))
+
+
+main()
