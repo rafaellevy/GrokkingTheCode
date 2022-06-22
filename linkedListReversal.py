38a7c603e2de62b989556294ad33b71d0c7af40d
rafaellevy <srlevy2@gmail.com>
2022-06-22 13:01:35 -0700

    Reverse a Sub-list done and tested

--- linkedListReversal.py
+++ linkedListReversal.py
@@ -1,3 +1,6 @@
+
+# Reverse a LinkedList
+
 '''
 Given the head of a Singly LinkedList, reverse the LinkedList. 
 Write a function to return the new head of the reversed LinkedList.
@@ -44,6 +47,63 @@ def main():
   print("Nodes of original LinkedList are: ", end='')
   head.print_list()
   result = reverse(head)
+  print("Nodes of reversed LinkedList are: ", end='')
+  result.print_list()
+
+
+main()
+
+#Reverse a Sub-list (medium)
+
+'''
+Given the head of a LinkedList and two positions p and q, 
+reverse the LinkedList from position p to q.
+
+'''
+
+
+def reverse_sub_list(head, p, q):
+  # TODO: Write your code here
+
+  leftNode = head
+  i = 1
+  while i < p - 1:
+    leftNode = leftNode.next
+    i += 1
+  startNode = leftNode.next
+
+
+  endNode = head
+  j = 1
+  while j < q:
+    endNode = endNode.next
+    j += 1
+  rightNode = endNode.next
+
+  
+  p1 = None
+  p2 = startNode
+  while p1 != endNode:
+    p3 = p2.next
+    p2.next = p1
+    p1 = p2
+    p2 = p3
+  leftNode.next = endNode
+  startNode.next = rightNode
+  
+  return head
+
+
+def main():
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+
+  print("Nodes of original LinkedList are: ", end='')
+  head.print_list()
+  result = reverse_sub_list(head, 2, 4)
   print("Nodes of reversed LinkedList are: ", end='')
   result.print_list()
 
