9ccc924322cd008277e12212a12bee087f351228
rafaellevy <srlevy2@gmail.com>
2022-06-22 11:44:54 -0700

    find_first_k_missing_positive done and tested

--- CyclicSort.py
+++ CyclicSort.py
@@ -402,11 +402,9 @@ Explanation: The smallest missing positive numbers are 1 and 2.
 
 '''
 
-
-print("hard")
 def swap(x,y,arr):
     arr[x], arr[y] = arr[y], arr[x]
-
+'''
 def find_first_k_missing_positive(nums, k):
     i = 0
     output = []
@@ -443,6 +441,39 @@ def find_first_k_missing_positive(nums, k):
             pointer += 1
             previousHigh = value
     return output
+'''
+
+
+            
+def find_first_k_missing_positive(nums, k):
+  n = len(nums)
+  i = 0
+  while i < len(nums):
+    j = nums[i] - 1
+    if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
+      nums[i], nums[j] = nums[j], nums[i]  # swap
+    else:
+      i += 1
+
+  missingNumbers = []
+  extraNumbers = set()
+  for i in range(n):
+    if len(missingNumbers) < k:
+      if nums[i] != i + 1:
+        missingNumbers.append(i + 1)
+        extraNumbers.add(nums[i])
+
+  # add the remaining missing numbers
+  i = 1
+  while len(missingNumbers) < k:
+    candidateNumber = i + n
+    # ignore if the array contains the candidate number
+    if candidateNumber not in extraNumbers:
+      missingNumbers.append(candidateNumber)
+    i += 1
+
+  return missingNumbers
+
 
 def main():
   print(find_first_k_missing_positive([3, -1, 4, 5, 5], 3))
@@ -451,8 +482,6 @@ def main():
 
 
 main()
-            
-
 
 
     
