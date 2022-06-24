296cd576f012adaa3d0ff49d904c1385509a9010
rafaellevy <srlevy2@gmail.com>
2022-06-24 12:48:59 -0700

    Reverse every K-element Sub-list done and tested

--- linkedListReversal.py
+++ linkedListReversal.py
@@ -125,3 +125,63 @@ def main():
 
 main()
 
+# Reverse every K-element Sub-list (medium)
+
+'''
+Given the head of a LinkedList and a number k, 
+reverse every k sized sub-list starting from the head.
+If, in the end, you are left with a sub-list with less than k elements, reverse it too.
+
+'''
+
+# start is the start node of the sublist
+def reverse_sublist_size_k(start, k, previous):
+  p1 = None
+  p2 = start
+  while k > 0 and p2 != None:
+    p3 = p2.next
+    p2.next = p1
+    p1 = p2
+    p2 = p3
+    k -= 1
+  
+  start.next = p3
+  if previous != None:
+    previous.next = p1
+
+  return p3, start
+
+def reverse_every_k_elements(head, k):
+  # TODO: Write your code here
+  newHead = head
+  i = 1
+  while i < k:
+    newHead = newHead.next
+    i+=1
+
+  currentNode = head
+  previousEnd = None
+  while currentNode != None:
+    currentNode, previousEnd = reverse_sublist_size_k(currentNode, k, previousEnd)
+
+  return newHead
+
+
+def main():
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+  head.next.next.next.next.next = Node(6)
+  head.next.next.next.next.next.next = Node(7)
+  head.next.next.next.next.next.next.next = Node(8)
+
+  print("Nodes of original LinkedList are: ", end='')
+  head.print_list()
+  result = reverse_every_k_elements(head, 3)
+  print("Nodes of reversed LinkedList are: ", end='')
+  result.print_list()
+
+
+main()
