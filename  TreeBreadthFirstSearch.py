a0e218ba3e7fd2d8831e15e25c65eff5914440a2
rafaellevy <srlevy2@gmail.com>
2022-07-15 13:06:27 -0700

    Depth First Search traversal done

---  TreeBreadthFirstSearch.py
+++  TreeBreadthFirstSearch.py
@@ -7,28 +7,46 @@ from left to right in separate sub-arrays.
 '''
 
 from collections import deque
+import queue
 
 
 class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left, self.right = None, None
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
 
 
 def traverse(root):
-  result = []
-  # TODO: Write your code here
-  return result
+    myQueue = [root]
+    result = []
+
+    while myQueue:
+        levelArray = []
+        level_length = len(myQueue)
+
+        for _ in range(level_length):
+            curNode = myQueue.pop(0)
+            levelArray.append(curNode.val)
+            if curNode.left:
+                myQueue.append(curNode.left)
+            if curNode.right:
+                myQueue.append(curNode.right)
+
+        result.append(levelArray)
+
+    
+
+    return result
 
 
 def main():
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  print("Level order traversal: " + str(traverse(root)))
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level order traversal: " + str(traverse(root)))
 
 
 main()
