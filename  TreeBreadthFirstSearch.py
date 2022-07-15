6cb4f3f4b26630396ba30d389c219f9a4709a465
rafaellevy <srlevy2@gmail.com>
2022-07-15 12:45:41 -0700

    pasted class and empty method

---  TreeBreadthFirstSearch.py
+++  TreeBreadthFirstSearch.py
@@ -6,3 +6,29 @@ You should populate the values of all nodes of each level
 from left to right in separate sub-arrays.
 '''
 
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def traverse(root):
+  result = []
+  # TODO: Write your code here
+  return result
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Level order traversal: " + str(traverse(root)))
+
+
+main()
