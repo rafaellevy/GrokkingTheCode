86d560f27b99cf1294d3c9fba12cda1a2d770637
rafaellevy <srlevy2@gmail.com>
2022-07-19 13:04:12 -0700

    connect level siblings debug

---  TreeBreadthFirstSearch.py
+++  TreeBreadthFirstSearch.py
@@ -1,3 +1,8 @@
+from __future__ import print_function
+from operator import length_hint
+import queue
+
+
 # Problem Statement
 
 '''
@@ -5,11 +10,6 @@ Given a binary tree, populate an array to represent its level-by-level traversal
 You should populate the values of all nodes of each level 
 from left to right in separate sub-arrays.
 '''
-
-from collections import deque
-from operator import length_hint
-import queue
-from turtle import left
 
 
 class TreeNode:
@@ -294,5 +294,71 @@ def main():
     result = find_successor(root, 12)
     if result:
         print(result.val)
+
+main()
+
+
+# Connect Level Order Siblings
+
+'''
+Given a binary tree, connect each node with its level order successor. 
+The last node of each level should point to a null node.
+'''
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right, self.next = None, None, None
+
+    # level order traversal using 'next' pointer
+    def print_level_order(self):
+        nextLevelRoot = self
+        while nextLevelRoot:
+            current = nextLevelRoot
+            nextLevelRoot = None
+        while current:
+            print(str(current.val) + " ", end='')
+        if not nextLevelRoot:
+            if current.left:
+                nextLevelRoot = current.left
+            elif current.right:
+                nextLevelRoot = current.right
+        current = current.next
+        print()
+
+
+def connect_level_order_siblings(root):
+    queue = [root]
+
+    while queue:
+        length = len(queue)
+        levelArray = []
+        for i in range(length):
+            node = queue.pop(0)
+            levelArray.append(node)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            if i == length - 1:
+                node.next = None
+        for j in range(len(levelArray) - 1):
+            levelArray[j].next = levelArray[j+1]
+
+    return root
+            
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    connect_level_order_siblings(root)
+
+    print("Level order traversal using 'next' pointer: ")
+    root.print_level_order()
+
 
 main()
