b3ebd8d75fac4f298296c8f37a1d996c1823660b
rafaellevy <srlevy2@gmail.com>
2022-07-19 12:37:32 -0700

    find successor done

---  TreeBreadthFirstSearch.py
+++  TreeBreadthFirstSearch.py
@@ -219,16 +219,6 @@ def find_minimum_depth(root):
                 queue.append(node.right)
                 
 
-    
-
-
-
-
-
-
-
-    return -1
-
 
 def main():
     root = TreeNode(12)
@@ -241,5 +231,68 @@ def main():
     root.right.left.left = TreeNode(11)
     print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
 
+
+main()
+
+
+# Level Order Successor
+
+'''
+Given a binary tree and a node, 
+find the level order successor of the given node in the tree. 
+The level order successor is the node that appears right after 
+the given node in the level order traversal.
+'''
+
+def find_successor(root, key):
+    # TODO: Write your code here
+    queue = [root]
+    result = []
+    ind = None
+
+    while queue:
+        length = len(queue)
+        for _ in range(length):
+            node = queue.pop(0)
+            result.append(node)
+            if node.val == key:
+                ind = len(result) - 1
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+    
+    if len(result) -1 == ind:
+        return -1
+
+    return result[ind+1]
+
+
+
+def main():
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+
+    result = find_successor(root, 3)
+    if result:
+        print(result.val)
+
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+
+    result = find_successor(root, 9)
+    if result:
+        print(result.val)
+
+    result = find_successor(root, 12)
+    if result:
+        print(result.val)
 
 main()
