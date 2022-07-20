1690282d54cd294406b22f8251a092d862cac139
rafaellevy <srlevy2@gmail.com>
2022-07-20 11:31:47 -0700

    Right View of a Binary Tree done

---  TreeBreadthFirstSearch.py
+++  TreeBreadthFirstSearch.py
@@ -423,3 +423,55 @@ def main():
 
 
 main()
+
+
+# Right View of a Binary Tree
+
+'''
+Given a binary tree, return an array containing nodes in its right view.
+ The right view of a binary tree is the set of nodes visible 
+ when the tree is seen from the right side.
+
+'''
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def tree_right_view(root):
+    result = []
+    # TODO: Write your code here
+    queue = [root]
+
+    while queue:
+        levelArray = []
+        length = len(queue)
+        for _ in range(length):
+            node = queue.pop(0)
+            levelArray.append(node)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        result.append(levelArray[-1])
+    return result
+
+    
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    root.left.left.left = TreeNode(3)
+    result = tree_right_view(root)
+    print("Tree right view: ")
+    for node in result:
+        print(str(node.val) + " ", end='')
+
+
+main()
