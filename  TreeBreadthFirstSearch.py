73b5f79a90d591c2d2b93806c89a1e370f545874
rafaellevy <srlevy2@gmail.com>
2022-07-20 11:15:18 -0700

    Connect All Level Order Siblings done

---  TreeBreadthFirstSearch.py
+++  TreeBreadthFirstSearch.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from operator import length_hint
+from operator import le, length_hint
 import queue
 
 
@@ -359,6 +359,67 @@ def main():
 
     print("Level order traversal using 'next' pointer: ")
     root.print_level_order()
+
+
+main()
+
+
+# Connect All Level Order Siblings 
+
+'''
+Given a binary tree, 
+connect each node with its level order successor.
+The last node of each level 
+should point to the first node of the next level.
+'''
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right, self.next = None, None, None
+ # tree traversal using 'next' pointer
+    def print_tree(self):
+        print("Traversal using 'next' pointer: ", end='')
+        current = self
+        while current:
+            print(str(current.val) + " ", end='')
+            current = current.next
+
+
+def connect_all_siblings(root):
+    # TODO: Write your code here
+    queue = [root]
+
+    while queue:
+        levelArray = []
+        length = len(queue)
+        for i in range(length):
+            node = queue.pop(0)
+            levelArray.append(node)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            if i == length - 1 and len(queue) != 0:
+                node.next = queue[0]
+        for j in range(length - 1):
+            levelArray[j].next = levelArray[j+1]
+
+    return root
+            
+            
+
+
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    connect_all_siblings(root)
+    root.print_tree()
 
 
 main()
