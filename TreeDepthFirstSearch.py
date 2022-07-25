b406245c612bfa63920de65ecbbb68ec5c733cc9
rafaellevy <srlevy2@gmail.com>
2022-07-25 13:01:33 -0700

    find_sum_of_path_numbers debug

--- TreeDepthFirstSearch.py
+++ TreeDepthFirstSearch.py
@@ -1,5 +1,8 @@
 # Binary Tree Path Sum
 
+from re import S
+
+
 class TreeNode:
     def __init__(self, val, left=None, right=None):
         self.val = val
@@ -80,6 +83,60 @@ def main():
     sum = 23
     print("Tree paths with sum " + str(sum) +
         ": " + str(find_paths(root, sum)))
+
+
+main()
+
+
+# Sum of Path Numbers
+
+'''
+Given a binary tree where each node can only have a digit (0-9) value, 
+each root-to-leaf path will represent a number. 
+Find the total sum of all the numbers represented by all paths.
+'''
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def find_sum_of_path_numbers(root):
+    # TODO: Write your code here
+    totalSum = 0
+    
+    return find_sum_of_path_numbers_helper(root,"", totalSum)
+
+
+def find_sum_of_path_numbers_helper(root,currentNumber, sum):
+    if root == None:
+        return 
+    
+    currentNumber += str(root.val)
+    
+    if root.left == None and root.right == None:
+        sum += int(currentNumber) 
+    
+    find_sum_of_path_numbers_helper(root.left, currentNumber,sum)
+    find_sum_of_path_numbers_helper(root.right, currentNumber,sum)
+    
+    currentNumber = currentNumber[:len(currentNumber)-1]
+    
+    
+    
+    
+
+
+def main():
+    root = TreeNode(1)
+    root.left = TreeNode(0)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(1)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(5)
+    print("Total Sum of Path Numbers: " + str(find_sum_of_path_numbers(root)))
 
 
 main()
