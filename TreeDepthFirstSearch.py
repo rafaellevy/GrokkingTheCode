f2bb7f7cafe2e53904d887c9df1d7d8fdac81bf2
rafaellevy <srlevy2@gmail.com>
2022-07-28 11:50:03 -0700

    Path With Given Sequence problem added

--- TreeDepthFirstSearch.py
+++ TreeDepthFirstSearch.py
@@ -125,13 +125,6 @@ def find_sum_of_path_numbers_helper(root,currentNumber, sum):
     
     return find_sum_of_path_numbers_helper(root.left, currentNumber,sum) + find_sum_of_path_numbers_helper(root.right, currentNumber,sum)
         
-    
-    
-    
-    
-    
-    
-
 
 def main():
     root = TreeNode(1)
@@ -141,6 +134,39 @@ def main():
     root.right.left = TreeNode(6)
     root.right.right = TreeNode(5)
     print("Total Sum of Path Numbers: " + str(find_sum_of_path_numbers(root)))
+
+
+main()
+
+# Path With Given Sequence 
+
+"""
+Given a binary tree and a number sequence,
+find if the sequence is present as a root-to-leaf path in the given tree.
+"""
+
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def find_path(root, sequence):
+  # TODO: Write your code here
+  return False  
+
+def main():
+
+  root = TreeNode(1)
+  root.left = TreeNode(0)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(1)
+  root.right.left = TreeNode(6)
+  root.right.right = TreeNode(5)
+
+  print("Tree has path sequence: " + str(find_path(root, [1, 0, 7])))
+  print("Tree has path sequence: " + str(find_path(root, [1, 1, 6])))
 
 
 main()
