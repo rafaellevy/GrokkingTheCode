5de38417b52733df54b9d0efc91d667e2c094dc3
Rafael Levy <srlevy2@gmail.com>
2022-08-03 11:17:16 -0700

    two heap file added

---
+++ .vscode/twoHeaps.py
@@ -0,0 +1,38 @@
+
+from heapq import heapify, heappop, heappush
+# Find the Median of a Number Stream
+'''
+Design a class to calculate the median of a number stream.
+ The class should have the following two methods:
+
+- insertNum(int num): stores the number in the class
+- findMedian(): returns the median of all numbers inserted in the class
+
+If the count of numbers inserted in the class is even, 
+the median will be the average of the middle two numbers.
+
+'''
+
+
+class MedianOfAStream:
+    def insert_num(self, num):
+        # TODO: Write your code here
+        return -1
+
+    def find_median(self):
+        # TODO: Write your code here
+        return 0.0
+
+
+def main():
+    medianOfAStream = MedianOfAStream()
+    medianOfAStream.insert_num(3)
+    medianOfAStream.insert_num(1)
+    print("The median is: " + str(medianOfAStream.find_median()))
+    medianOfAStream.insert_num(5)
+    print("The median is: " + str(medianOfAStream.find_median()))
+    medianOfAStream.insert_num(4)
+    print("The median is: " + str(medianOfAStream.find_median()))
+
+
+main()
