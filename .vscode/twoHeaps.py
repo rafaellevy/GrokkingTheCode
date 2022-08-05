049264fd90dd458355c1e324b3a499999602abdc
Rafael Levy <srlevy2@gmail.com>
2022-08-05 12:34:51 -0700

    Sliding Window Median Heap basic structure

--- .vscode/twoHeaps.py
+++ .vscode/twoHeaps.py
@@ -2,6 +2,7 @@
 from heapq import heapify, heappop, heappush
 from re import S
 from statistics import median
+from unittest import result
 # Find the Median of a Number Stream
 '''
 Design a class to calculate the median of a number stream.
@@ -65,6 +66,91 @@ def main():
     print("The median is: " + str(medianOfAStream.find_median()))
     medianOfAStream.insert_num(4)
     print("The median is: " + str(medianOfAStream.find_median()))
+
+
+main()
+
+# Sliding Window Median
+
+"""
+    Given an array of numbers and a number k, 
+    find the median of all the k sized sub-arrays 
+    (or windows) of the array.
+    """
+
+
+class SlidingWindowMedian:
+    def __init__(self) -> None:
+        # minheap should contain the second half of the array ( larger half)
+        self.minHeap = []
+        self.maxHeap = []
+        self.result = []
+
+    def find_sliding_window_median(self, nums, k):
+        # TODO: Write your code here
+        windowStart = 0
+        windowEnd = 0
+
+        # add first number to heap
+        self.insert_num(nums[windowEnd])
+
+        for i in range(len(nums)):
+            if windowEnd - windowStart + 1 == k:
+                self.result.append(self.find_median(
+                    nums[windowStart:windowEnd + 1]))
+                # TODO: remove the start number from min or max heap
+                windowStart += 1
+
+            windowEnd += 1
+            self.insert_num(nums[windowEnd])
+
+        return self.result
+
+    def insert_num(self, num):
+
+        # TODO: Write your code here
+        if len(self.minHeap) == 0 and len(self.maxHeap) == 0:
+            heappush(self.minHeap, num)
+        elif num > self.minHeap[0]:
+            heappush(self.minHeap, num)
+        else:
+            heappush(self.maxHeap, -num)
+
+        if len(self.minHeap) > len(self.maxHeap) + 1:
+            # rebalancing
+            topMinHeap = heappop(self.minHeap)
+            # max heap is a min heap ( add - sign)
+            heappush(self.maxHeap, -topMinHeap)
+
+        elif len(self.maxHeap) > len(self.minHeap):
+            topMaxHeap = heappop(self.maxHeap)
+            # max heap is a min heap ( add - sign to go back to positive)
+            heappush(self.minHeap, -topMaxHeap)
+
+    def find_median(self, window):
+
+        print(self.minHeap)
+        print(self.maxHeap)
+        # TODO: Write your code here
+        if len(self.minHeap) > len(self.maxHeap):
+            return self.minHeap[0]
+        else:
+            resultMedian = (self.minHeap[0] + -self.maxHeap[0]) / 2
+
+            return resultMedian
+
+
+def main():
+
+    slidingWindowMedian = SlidingWindowMedian()
+    result = slidingWindowMedian.find_sliding_window_median(
+        [1, 2, -1, 3, 5], 2)
+    print("Sliding window medians are: " + str(result))
+
+    slidingWindowMedian = SlidingWindowMedian()
+    result = slidingWindowMedian.find_sliding_window_median(
+        [1, 2, -1, 3, 5], 3)
+    print("Sliding window medians are: " + str(result))
 
 
 main()
