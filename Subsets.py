4d58c89c6f028394d85e7f249ee6dc1becbca2ad
Rafael Levy <srlevy2@gmail.com>
2022-08-18 12:19:13 -0700

    permutations with recusion done

--- Subsets.py
+++ Subsets.py
@@ -113,3 +113,30 @@ def main():
 
 
 main()
+
+
+# Permutations (recursively)
+
+def find_permutations(nums):
+    result = []
+    generate_permutations_recursive(nums, 0, [], result)
+    return result
+
+
+def generate_permutations_recursive(nums, index, currentPermutation, result):
+    if index == len(nums):
+        result.append(currentPermutation)
+    else:
+        for i in range(len(currentPermutation)+1):
+            newPermutation = list(currentPermutation)
+            newPermutation.insert(i, nums[index])
+            generate_permutations_recursive(
+                nums, index + 1, newPermutation, result)
+
+
+def main():
+    print("Here are all the permutations: " +
+          str(find_permutations([1, 3, 5])))
+
+
+main()
